e44afb262544597a139d689bf51dcb9b00730c4f
Bill Hess <bill@abuno.com>
2024-04-12 17:08:51 -0400

    EDC initial commit
    Adding in style adjustments and key maps

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -153,6 +153,24 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+
+-- -- [[ My Stuff ]] -- --
+
+-- [[ Mac/emacs bindings for insert mode]]
+vim.keymap.set('i', '<C-e>', '<C-o>$')
+vim.keymap.set('i', '<C-a>', '<C-o>0')
+vim.keymap.set('i', '<C-b>', '<Left>')
+vim.keymap.set('i', '<C-f>', '<Right>')
+vim.keymap.set('i', '<C-d>', '<Delete>')
+vim.keymap.set('i', '<C-k>', '<C-o>D')
+vim.keymap.set('i', ';;', '<Esc>')
+
+-- Try this so you really need to mean to record something
+-- Playback is still @ + recording letter
+vim.keymap.set('n', '<Leader>q', 'q')
+vim.keymap.set('n', 'q', '<Nop>')
+
+-- ====================================== --
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -714,13 +732,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      --vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'nightfox'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
