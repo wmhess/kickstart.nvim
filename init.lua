d2823000523b25ff027dce49db417125c3c60f18
Bill Hess <bill@wmhess.com>
2026-04-06 14:13:25 -0400

    edc
    removed the call to stylua

--- init.lua
+++ init.lua
@@ -684,8 +684,6 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
-        stylua = {}, -- Used to format Lua code
-
         -- Special Lua Config, as recommended by neovim help docs
         lua_ls = {
           on_init = function(client)
