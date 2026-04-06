32b1ac6523a009181002f8dd79064e0fe8655e8f
Bill Hess <bill@wmhess.com>
2026-04-06 13:35:34 -0400

    Alt test change
    trying the tree sitter update

--- init.lua
+++ init.lua
@@ -946,6 +946,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
